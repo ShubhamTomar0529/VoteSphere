3ce29d6e0443da09537d51f7056abab8523d3fee
Shubham Tomar <shubhamtomar0529@gmail.com>
2026-03-10 23:03:38 +0530

    Add files via upload

---
+++ Web-Based-Online-Voting-System/Online_Voting_System/voting/admin/can-delete.php
@@ -0,0 +1,18 @@
+<?php
+include "../../../config.php";
+?>  
+<?php
+$id=$_GET['id'];;
+
+$con=mysqli_connect('localhost','root','','voting');
+$query="DELETE FROM candidate WHERE id='$id'";
+$data=mysqli_query($con,$query);
+
+if($data)
+{
+    echo "<script>
+            alert('candidate deleted!')
+            history.back()
+         </script>";
+}
+?>
---
+++ Web-Based-Online-Voting-System/Online_Voting_System/voting/admin/voter_req_delete.php
@@ -0,0 +1,15 @@
+<?php
+include "../../../config.php";
+?>  
+<?php
+
+            $id=$_GET['id'];
+            $con=mysqli_connect('localhost','root','','voting');
+            $query="DELETE FROM phno_change WHERE id='$id'";
+            $data=mysqli_query($con,$query);
+
+            if($data)
+            {
+                echo "<script> history.back()</script>";
+            }
+        ?>
